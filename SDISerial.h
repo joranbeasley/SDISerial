5be6aa4b2115468c4a8fc884a87b873da44e66b0
Joran Beasley <joranbeasley@gmail.com>
2014-03-04 07:47:35 -0800

    added docstrings for doxygen docs

--- SDISerial.h
+++ SDISerial.h
@@ -44,6 +44,13 @@ http://arduiniana.org.
 #define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 #endif
 inline void DebugPulse(uint8_t pin, uint8_t count);
+
+//! SDISerial
+/*!
+   An Arduino library to facilitate simple communication with SDI-12 Devices, that
+   are common with soil moisture sensors
+   \author Joran Beasley
+*/
 class SDISerial : public Stream
 {
 private:
@@ -66,7 +73,7 @@ private:
   //sdi12 stuff 
   char response[255];
   bool response_ready;
-  char response_complete_byte;
+  
   // static data
   static char _receive_buffer[_SS_MAX_RX_BUFF]; 
   static volatile uint8_t _receive_buffer_tail;
@@ -97,16 +104,91 @@ private:
   
 public:
   // public methods
+  //!Constructor
+  /*!
+     \param dataPin : The pin you have connected to the dataline
+     \param inverse_logic(optional:TRUE) : inverted signal should always be true for SDI-12
+     \returns a new SDISerial Connection
+     
+     \code{.cpp}
+	  #include <SDISerial.h>
+	  #define DATA_PIN 2
+	  SDISerial sdi(DATA_PIN);
+	  \endcode
+  */
   SDISerial(uint8_t dataPin, bool inverse_logic = true);
-  ~SDISerial(){end();}
+  
+
+  //!Initializer
+  /*!
+    MUST be called before attempting any communication
+    \code{.cpp}
+	  void setup(){
+			sdi.begin();
+	   }
+	  \endcode
+  */
   void begin();
   
+  
+  //!Send Command To SDI-12 Sensor Bus
+  /*!
+    \param bytes : The query string to send
+    \code{.cpp}
+	  sdi.sdi_cmd("0A1!"); // change address from 0 to 1 
+	  \endcode
+  */
   void sdi_cmd(const char* bytes);
-  char* sdi_query(const char* bytes,uint32_t timeout_ms);
+  
+  
+  //!Read a response string from the sensor based on a command query \see  response_complete_byte
+  /*!
+    \param timeout_ms : the maximum time to wait for a response in miliseconds
+    \returns the response string or NULL in the event of a timeout
+    \code{.cpp}
+	  sdi.sdi_cmd("0I!"); // get info from sensor 0
+	  char* sensor_info = sdi.wait_for_response(1000); // wait 1 second maximum
+	  
+	  \endcode
+  */
   char* wait_for_response(uint32_t timeout_ms);
-  char* service_request(const char* service_request,const char* read_command);
+  
+  
+  //! query the sensor and wait for a response. \see  response_complete_byte
+  /*!
+    \param bytes : The query string to send
+    \param timeout_ms : the maximum time to wait for a response in miliseconds
+    \returns the response string or NULL in the event of a timeout
+    \code{.cpp}
+	  char* sensor_info = sdi.sdi_query("0I!",1000); // get info from sensor 0
+	  \endcode
+  */
+  char* sdi_query(const char* bytes,uint32_t timeout_ms);
+  
+  
+  //! convience function that takes advantage of service requests 
+  //! service requests are requests that are returned in stages  "?M!" is typically the service request command
+  /*!
+    \param serviceRequest : The query string to initiate the service request(typically `"?M!"`)
+    \param readCommand : the command that fetches the request once the sensor has provided it (typically "?D0!")
+    \returns the response string from the read_command or NULL in the event of a timeout
+    \code{.cpp}
+	  char* current_measurement = sdi.service_request("0M!","0D0!"); // get measurement from sensor 0
+	\endcode
+  */
+  char* service_request(const char* serviceRequest,const char* readCommand);
+  
+  char response_complete_byte; /*!< specifies the byte that signifies sensor has finished the message 
+                                    default value is '\n'
+                                    \code{.cpp}
+                                    sdi.response_complete_byte = '\r'; 
+									\endcode
+                               */
+  //\cond HIDDEN_SYMBOLS
+  ~SDISerial(){end();}
   // public only for easy access by interrupt handlers
   static inline void handle_interrupt();
+  //\endcond
   
 };
 
